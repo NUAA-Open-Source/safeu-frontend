c5d0df6b819367d78dd777f32664be9346fcd946
ninjiacoder <ninjiacoder@gmail.com>
2019-01-26 10:04:56 +0800

    initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="navbar">
+        <v-toolbar>
+            <v-toolbar-side-icon></v-toolbar-side-icon>
+            <v-toolbar-title>
+                <v-avatar
+                    slot="activator"
+                    size="36px"
+                >
+                    <img src="../assets/nuaa_logo.jpeg"/>
+                </v-avatar>
+                safeu 云U盘</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn flat>更多项目</v-btn>
+            <v-btn flat>关于我们</v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style>
+#navbar {
+    width: 100%;
+    height: 60px;
+}
+
+#nuaa-logo {
+
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
+import './plugins/vuetify'
+import Vuetify from 'vuetify'
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
 import App from './App.vue'
+
+Vue.use(Antd)
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
