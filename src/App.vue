8f5e53cd24c647458a40f0734fc9b2e69e06204a
ninjiacoder <ninjiacoder@gmail.com>
2019-02-04 12:36:46 +0800

    UI: rewrite the UI of choose files

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,8 @@ export default {
 </script>
 <style>
 #app {
+  width: 100%;
+  height: 100%;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/main.js
+++ src/main.js
@@ -4,9 +4,14 @@ import Vuetify from 'vuetify'
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/antd.css";
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCloudUploadAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.use(Antd)
 Vue.use(Vuetify)
+library.add(faCloudUploadAlt)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
