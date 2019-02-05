77cf13c9f719a4d121ca66adfe54e6bf5bf8c57c
ninjiacoder <ninjiacoder@gmail.com>
2019-02-05 14:51:45 +0800

    improve: import vue-router to manage router

--- src/UploadView.vue
+++ src/UploadView.vue
@@ -1,15 +1,23 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+    <div id="uploadview">
+        <Navbar/>
+        <upload/>
+    </div>
 </template>
 <script>
+import Navbar from './components/Navbar.vue'
+import upload from './components/upload.vue'
+
 export default {
-  name: 'app',
+    name: 'uploadview',
+    components: {
+        Navbar,
+        upload,
+    }
 }
 </script>
 <style>
-#app {
+#uploadview {
   width: 100%;
   height: 100%;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
--- src/main.js
+++ src/main.js
@@ -4,17 +4,29 @@ import Vuetify from 'vuetify'
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/antd.css";
 import App from './App.vue'
+import UploadView from './UploadView.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCloudUploadAlt } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import VueRouter from 'vue-router'
 
 Vue.use(Antd)
 Vue.use(Vuetify)
+Vue.use(VueRouter)
 library.add(faCloudUploadAlt)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
+let router = new VueRouter({
+  routes: [
+    {path: '/', component: UploadView},
+  ],
+  mode: 'history',
+})
+
 new Vue({
+  el: '#app',
+  router,
   render: h => h(App),
 }).$mount('#app')
