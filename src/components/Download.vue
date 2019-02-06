cabb4a196632d45aec75f13a9d56f831a2656609
ninjiacoder <ninjiacoder@gmail.com>
2019-02-06 16:21:14 +0800

    add password input when validation request status is 401

--- src/components/Download.vue
+++ src/components/Download.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="download-container">
         <p class="download-top-hint">输入提取码</p>
-        <input class="download-input" v-model="recode">
+        <input class="download-recode-input" v-model="recode">
+        <input type="password" class="download-password-input" v-if="needpassword" v-model="password" placeholder="输入密码">
         <button class="download-button" v-on:click="display()">提取</button>
     </div>
 </template>
@@ -12,12 +13,14 @@ export default {
     data() {
         return {
             recode: '',
+            needpassword: false,
+            password: '',
         }
     },
     methods: {
         display() {
             var sha256 = require("js-sha256").sha256
-            var password_sha256 = sha256(this.recode)
+            var password_sha256 = sha256(this.recode + sha256(this.password))
             this.sendValidationRequest(password_sha256)
         },
 
@@ -25,11 +28,15 @@ export default {
             const xmlhttp = new XMLHttpRequest();
             xmlhttp.open("POST", "", true)
             xmlhttp.onreadystatechange = function() {
-                if (xmlhttp.readyState == XMLHttpRequest.DONE && xmlhttp.status == 200) {
-                    var token = JSON.parse(xmlhttp.response).token
-                    var file_items = JSON.parse(xmlhttp.response).items
-                    window.localStorage.setItem('token', token)
-                    this.$router.push({path: '/filelist', query: {files: file_items}})
+                if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+                    if  (xmlhttp.status == 200) {
+                        var token = JSON.parse(xmlhttp.response).token
+                        var file_items = JSON.parse(xmlhttp.response).items
+                        window.localStorage.setItem('token', token)
+                        this.$router.push({path: '/filelist', query: {files: file_items}})
+                    } else if (xmlhttp.status == 401) {
+                        this.needpassword = true
+                    }
                 }
             }
             xmlhttp.send(JSON.stringify({"password": password}))
