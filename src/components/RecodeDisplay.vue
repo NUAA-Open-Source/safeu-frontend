645f15ce36267909a3f33140567a90095d12b8cb
ninjiacoder <ninjiacoder@gmail.com>
2019-02-13 10:27:37 +0800

    add alert when edit recode

--- src/components/RecodeDisplay.vue
+++ src/components/RecodeDisplay.vue
@@ -3,11 +3,14 @@
         <div class="recode-container" v-if="!is_more_setting">
             <p class="recode-top-hint">您的提取码</p>
             <div class="recode-input-row">
-                <input id="recode-box" class="recode-box" v-model="new_recode" :disabled="!is_editting_recode"/>
+                <input id="recode-box" class="recode-box" v-model="new_recode" :disabled="!is_editting_recode" @focus="inputonfocus"/>
                 <a v-on:click="editrecode" v-if="!is_editting_recode">修改</a>
                 <a v-on:click="finisheditrecode" v-else>完成</a>
             </div>
-            <vue-qr :text="qrcode_url" style="margin-top: 20px"></vue-qr>
+            <a-alert class="alert" message="设置成功" type="success" showIcon v-if="alert_style == 1"/>
+            <a-alert class="alert" message="该提取码已存在" type="error" showIcon v-else-if="alert_style == 2"/>
+            <a-alert class="alert" message="设置失败" type="error" showIcon v-else-if="alert_style == 3"/>
+            <vue-qr :text="qrcode_url" style="margin-top: 8px"></vue-qr>
             <p>您可以保存或分享此二维码</p>
             <a v-on:click="moresetting">> 更多设置</a>
         </div>
@@ -38,6 +41,7 @@
                 is_editting_recode: false,
                 is_more_setting: false,
                 is_need_password: false,
+                alert_style: 0,
                 password: '',
                 new_recode: this.recode,
             }
@@ -47,13 +51,30 @@
                 this.is_editting_recode = true
             },
 
+            inputonfocus() {
+                this.alert_style = 0;
+            },
+
             finisheditrecode() {
                 this.is_editting_recode = false
+                var that = this
                 if (this.recode != this.new_recode) {
                     var xhr = new XMLHttpRequest()
                     var user_token = window.localStorage.getItem('owner_token')
                     xhr.open("POST", _global.domain_url + "recode/" + this.recode, true)
                     xhr.onreadystatechange = function() {
+                        if (xhr.readyState == XMLHttpRequest.DONE) {
+                            if (xhr.status == 200) {
+                                that.alert_style = 1;
+                                
+                            } else if (xhr.status == 400) {
+                                if (JSON.parse(xhr.response).message == "reCode Repeat") {
+                                    that.alert_style = 2;
+                                } else {
+                                    that.alert_style = 3;
+                                }
+                            }
+                        }
                         console.log(xhr.response)
                     }
                     xhr.send(JSON.stringify({"new_re_code": this.new_recode, "user_token": user_token}))
