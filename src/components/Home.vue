b61d774767db8589a84e586cc5b3e141fa3af7b4
Villivateur <villivateur@gmail.com>
2019-02-15 14:52:17 +0800

    add Home Page

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="buttons-container">
+      <a class="main-buttons" href="/upload">上传</a>
+      <a class="main-buttons" href="/download">下载</a>
+    </div>
+    <div class="imgs-container">
+      <hr style="margin:5px 8% 5px">
+      <img style="height: auto; width: 350px" src="https://ftp.a2os.club/img/a2os.png">
+      <!-- 在这里放图片，主要是想以后可以在这里加一些宣传语或者广告什么的。但是发现我tcl，多端适配都搞不好。 --vvzero -->
+    </div>
+    <footer style="text-align: center; margin: 60px 0px 10px">
+      <p>&copy; a2os.club 2018-2019 All rights reserved</p>
+    </footer>
+  </div>
+</template>
+
+<style>
+@import "./Home.css";
+</style>
