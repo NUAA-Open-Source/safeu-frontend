961c9a499c3e986f38486b8195bd35958e7d4a4d
ninjiacoder <ninjiacoder@gmail.com>
2019-02-16 17:18:15 +0800

    add uploaded files show

--- src/UploadView.vue
+++ src/UploadView.vue
@@ -2,17 +2,20 @@
     <div id="uploadview">
         <Navbar/>
         <upload/>
+        <UploadedFiles/>
     </div>
 </template>
 <script>
 import Navbar from './components/Navbar.vue'
 import upload from './components/upload.vue'
+import UploadedFiles from './components/UploadedFiles.vue'
 
 export default {
     name: 'uploadview',
     components: {
         Navbar,
         upload,
+        UploadedFiles,
     }
 }
 </script>
