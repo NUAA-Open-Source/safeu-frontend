46b4379206e7c07ead5318b002ae5b8a7b412b25
ninjiacoder <ninjiacoder@gmail.com>
2019-02-17 16:00:48 +0800

    change Navbar style and edit more setting position

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,21 +1,9 @@
 <template>
     <div id="navbar">
-        <v-toolbar>
-            <v-toolbar-side-icon></v-toolbar-side-icon>
-            <v-toolbar-title>
-                <v-avatar
-                    slot="activator"
-                    size="36px"
-                >
-                    <img src="../assets/nuaa_logo.jpeg"/>
-                </v-avatar>
-                safeu 云U盘</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-toolbar-items class="hidden-sm-and-down">
-            <v-btn flat>更多项目</v-btn>
-            <v-btn flat>关于我们</v-btn>
-            </v-toolbar-items>
-        </v-toolbar>
+        <div class="logo">
+            <a class="logo-name" href="/">SafeU</a>
+            <p class="logo-subname">云U盘</p>
+        </div>
     </div>
 </template>
 
@@ -26,13 +14,5 @@ export default {
 </script>
 
 <style>
-#navbar {
-    width: 100%;
-    height: 60px;
-}
-
-#nuaa-logo {
-
-}
-
+@import './Navbar.css';
 </style>
