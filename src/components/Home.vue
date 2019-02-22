6fe79e6ed79e603db3708f4464cb5153c13a3178
Villivateur Von <villivateur@vvzero.com>
2019-02-22 13:07:32 +0800

    modify words in the home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
   <div>
     <div id="intro">
       <p style="font-size: 35px; font-weight: bold">南航人自己的云U盘</p>
-      <p style="font-size: 15px;">将你的文件上传至此，而后你可以在任一联网的设备下载该文件。具体文件限制请查看使用须知。</p>
+      <p style="font-size: 15px;">这里是 SafeU 云U盘，一款好用、快捷、安全的文件分享服务。</p>
     </div>
     <div class="buttons-container">
       <a class="main-buttons" href="/upload">上传</a>
