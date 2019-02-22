1da14edaa5250c6c5646de351cbe4affcc88be16
ninjiacoder <ninjiacoder@gmail.com>
2019-02-22 21:42:52 +0800

    improve home page again

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,11 +4,13 @@
       <p class="intro-title name">SafeU</p>
       <p class="intro-title">好用、快捷、安全</p>
       <p class="intro-title">的文件分享服务</p>
-      <p class="intro-subtitle">我们提供了非常高效及安全的文件传输服务，完胜并取代 90% 的 U 盘应用场景</p>
     </div>
-    <div class="buttons-container">
-      <a class="main-buttons" href="/upload">上传</a>
-      <a class="main-buttons" href="/download">下载</a>
+    <div class="intro-sub">
+      <p class="intro-subtitle">我们提供了非常高效及安全的文件传输服务，完胜并取代 90% 的 U 盘应用场景</p>
+      <div class="buttons-container">
+        <a class="main-upload-buttons" href="/upload"><font-awesome-icon icon="arrow-alt-circle-up" size="1x"/> 上传</a>
+        <a class="main-download-buttons" href="/download"><font-awesome-icon icon="arrow-alt-circle-down" size="1x"/> 下载</a>
+      </div>
     </div>
   </div>
 </template>
