9361237937015e8d03f7e3c1af0833f9623b1a9c
ninjiacoder <ninjiacoder@gmail.com>
2019-04-14 19:56:54 +0800

    <feat> add vue.config.js to manage different environment and improve event log

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    publicPath: '/',
+    outputDir: process.env.outputDir,
+    assetsDir: 'static',
+    pages: {
+        index: {
+            entry: process.env.NODE_ENV === 'production'
+                ? './src/main.prod.js'
+                : './src/main.dev.js',
+            template: process.env.NODE_ENV === 'production'
+                ? './public/index.prod.html'
+                : './public/index.dev.html'
+        }
+    },
+}
