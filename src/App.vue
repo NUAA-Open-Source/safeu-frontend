b54e3d89a2c6c038b8179d98ed86035ddb80b385
ninjiacoder <ninjiacoder@gmail.com>
2019-05-27 18:28:39 +0800

    #40 <fix> fix dynamic url domain

--- src/App.vue
+++ src/App.vue
@@ -26,12 +26,12 @@ export default {
   },
   mounted() {
     this.$axios.defaults.withCredentials = true
-    this.$axios.get(_global.domain_url + "csrf")
+    this.$axios.get(_global.api_domain_url + "csrf")
     .then(function(response) {
       var csrf_token = response.headers["x-csrf-token"]
       sessionStorage.setItem("csrf_token", csrf_token)
     })
-    this.$axios.get(_global.behavior_domain_url + "csrf")
+    this.$axios.get(_global.behavior_api_domain_url + "csrf")
     .then(function(response) {
       var csrf_token = response.headers["x-csrf-token"]
       sessionStorage.setItem("behavior_csrf_token", csrf_token)
--- src/Global.vue
+++ src/Global.vue
@@ -1,14 +1,19 @@
 <script>
-const domain_url = process.env.VUE_APP_CURRENTMODE === 'production'
+const api_domain_url = process.env.VUE_APP_CURRENTMODE === 'production'
     ? 'https://api.safeu.a2os.club/'
     : process.env.VUE_APP_CURRENTMODE === 'staging'
     ? 'https://testapi.safeu.a2os.club/'
     : 'http://114.115.169.206:9000/'
-const behavior_domain_url = process.env.VUE_APP_CURRENTMODE === 'production'
+const behavior_api_domain_url = process.env.VUE_APP_CURRENTMODE === 'production'
     ? 'https://api.behavior.a2os.club/'
     : process.env.VUE_APP_CURRENTMODE === 'staging'
     ? 'https://testapi.behavior.a2os.club/'
     : 'http://114.115.169.206:9005'
+const domain_url = process.env.VUE_APP_CURRENTMODE === 'production'
+    ? 'https://safeu.a2os.club/'
+    : process.env.VUE_APP_CURRENTMODE === 'staging'
+    ? 'https://test.safeu.a2os.club/'
+    : ''
 const version = 'v1/'
 const behavior_version = 'v1/'
 const error_code = {
@@ -31,13 +36,13 @@ const error_code = {
     20307: '提取码重复',
     20501: '密码错误'
 }
-const api_url = domain_url + version
-const behavior_api_url = behavior_domain_url + behavior_version
+const api_url = api_domain_url + version
+const behavior_api_url = behavior_api_domain_url + behavior_version
 export default {
-    domain_url,
+    api_domain_url,
     version,
     api_url,
-    behavior_domain_url,
+    behavior_api_domain_url,
     behavior_api_url,
     error_code,
 }
